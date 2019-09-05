f38780ef6b73fff1716e3eec66c7b9e588307bf1
Brett Pasula <BPasula@copperleaf.com>
2019-09-05 09:59:51 -0700

    Version one.

---
+++ EssentialTraining/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTraining
+{ 
+    // Simple string list class. 
+    public class MyList
+    {
+        public List<string> Elements;
+
+        // Constructs a MyList of given size. 
+        public MyList(int size)
+        {
+            Elements = new List<string>(size); 
+        }
+
+        // Returns false if Elements is not empty, and true if it is.
+        public bool IsEmpty()
+        {
+            if (Elements.Count == 0)
+            {
+                return true; 
+            }
+            else
+            {
+                return false; 
+            }
+        }
+
+        // Adds given string to Elements. 
+        public void Add(string str)
+        {
+            Elements.Add(str);
+        }
+    }
+}
---
+++ EssentialTraining/SimpleArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace EssentialTraining
+{ 
+    // Simple string array class. 
+    public class SimpleArray
+    {
+        public string[] GroceryList;
+
+        // Constructs a SimpleArray of given size.
+        public SimpleArray(int size)
+        {
+            GroceryList = new string[size];
+        }
+
+        // Prints out the contents of Grocery List in the format "There are [GroceryList.Length] items and they are
+        // [Item1, Item2, Item3, etc.]." 
+        public override string ToString()
+        {
+            return String.Format("There are {0:D} items and they are {1}.", GroceryList.Length, GroceryList);
+        }
+
+        // Adds given string at the given index. 
+        public void Add(string str, int index)
+        {
+            GroceryList[index] = str; 
+        }
+
+        // Returns the item at the given index. 
+        public string Get(int index)
+        {
+            return GroceryList[index]; 
+        }
+    }
+}
