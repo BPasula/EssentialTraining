558007bd9942bf648f2852ed1e78385f2981334f
Brett Pasula <BPasula@copperleaf.com>
2019-09-05 10:58:50 -0700

    Added a simple console app to illustrate exception handling in C#.

---
+++ EssentialTrainingApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.IO; 
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static List<string> Words;
+        static void Main(string[] args)
+        {  
+            Words = new List<string>();
+            Words.Add("Bread");  
+            Words.Add("Milk"); 
+            Words.Add("Cheese");
+
+            CrazyMathProblem();  
+            ReadTextFile();
+            Console.ReadLine(); 
+        }
+
+        private static int CrazyMathProblem()
+        {
+            var income = 1000;
+            for (var i = 10; i > 0; i--)
+            {
+                income = (income / i); 
+            }
+
+            return income; 
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine((contents));
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the directory. Error: " + ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the file. Error: " + ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unknown error. Error: " + ex);
+            }
+            finally
+            {
+                Console.WriteLine("Finally runs no matter what.");
+            }
+        }
+    }
+}
